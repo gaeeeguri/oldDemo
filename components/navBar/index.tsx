ae0888a1b8c960797b8feeac7cf06d6caa50890c
gaeeeguri <sjpark513@gm.gist.ac.kr>
2024-04-05 20:45:58 +0900

    resize navBar

--- components/navBar/index.tsx
+++ components/navBar/index.tsx
@@ -7,25 +7,25 @@ import { MdHome, MdPerson, MdSearch } from 'react-icons/md';
 export function NavBar() {
   return (
     <>
-      <footer className='flex justify-center bottom-0 sticky bg-primary h-16 items-center'>
-        <nav className='container flex items-center justify-between px-12 py-4 mx-auto font-semibold leading-6 lg:py-6 bottom-0 h-1 border-t-2 border-b-blue-300 max-w-md'>
+      <footer className='flex content-center justify-center bottom-0 sticky bg-primary h-16 items-center'>
+        <nav className='container flex content-center items-center justify-between px-12 py-4 mx-auto font-semibold leading-6 lg:py-6 bottom-0 border-t-2 border-b-blue-300 max-w-md'>
           <NavLink
             href={'/search'}
-            icon={<Icon as={MdSearch} />}
+            icon={<Icon boxSize={26} as={MdSearch} />}
             name={'Search'}
             exact
             className='transition duration-300 text-hover'
           />
           <NavLink
             href={'/'}
-            icon={<Icon as={MdHome} />}
+            icon={<Icon boxSize={26} as={MdHome} />}
             name={'Main'}
             exact
             className='transition duration-300 text-hover'
           />
           <NavLink
             href={'/profile'}
-            icon={<Icon as={MdPerson} />}
+            icon={<Icon boxSize={26} as={MdPerson} />}
             name={'Profile'}
             exact
             className='transition duration-300 text-hover'
